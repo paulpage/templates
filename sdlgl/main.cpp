9cedab154b6c4583eb70ecaa53fcf0339a76725f
Paul Page <pjpage98@gmail.com>
2022-01-02 19:15:08 -0600

    Add rounded rectangle

--- sdlgl/main.cpp
+++ sdlgl/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 
 typedef struct AppState {
     float window_width;
@@ -102,6 +103,9 @@ bool app_init() {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
+
     state.window = SDL_CreateWindow("App", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, (int)state.window_width, (int)state.window_height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (state.window == NULL) {
         printf("Error creating window: %s\n", SDL_GetError());
@@ -123,6 +127,9 @@ bool app_init() {
     if (SDL_GL_SetSwapInterval(1) < 0) {
         printf("Warning: Unable to set VSync: %s\n", SDL_GetError());
     }
+
+
+    glEnable(GL_MULTISAMPLE);
 
     // GL Programs
     state.tri_program_id = gl_create_program(TRI_VERT_SRC, TRI_FRAG_SRC);
@@ -147,16 +154,24 @@ void app_quit() {
     SDL_Quit();
 }
 
-void gl_draw_triangles(GLfloat vertex_data[], GLuint index_data[]) {
+float gl_x(float x) {
+    return -1.0f + 2.0f * x / state.window_width;
+}
+
+float gl_y(float y) {
+    return 1.0f - 2.0f * y / state.window_height;
+}
+
+void gl_draw_triangles(GLfloat vertex_data[], GLuint index_data[], int vertex_count, int triangle_count) {
     GLuint vbo, ibo;
     GLint vertex_pos_location = -1, vertex_color_location = -1;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 4 * sizeof(GLfloat), vertex_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * vertex_count * sizeof(GLfloat), vertex_data, GL_STATIC_DRAW);
 
     glGenBuffers(1, &ibo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLuint), index_data, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * triangle_count * sizeof(GLuint), index_data, GL_STATIC_DRAW);
 
     vertex_pos_location = glGetAttribLocation(state.tri_program_id, "position");
     vertex_color_location = glGetAttribLocation(state.tri_program_id, "color");
@@ -171,16 +186,16 @@ void gl_draw_triangles(GLfloat vertex_data[], GLuint index_data[]) {
     glEnableVertexAttribArray(vertex_color_location);
     glVertexAttribPointer(vertex_color_location, 4, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(2*sizeof(GLfloat)));
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+    glDrawElements(GL_TRIANGLES, 3 * triangle_count, GL_UNSIGNED_INT, NULL);
     glDisableVertexAttribArray(vertex_pos_location);
     glUseProgram(0);
 }
 
 void gl_draw_rect(float x, float y, float w, float h, Color color) {
-    float x1 = -1 + 2 * x / state.window_width;
-    float y1 = 1 - 2 * y / state.window_height;
-    float x2 = -1 + 2 * (x + w) / state.window_width;
-    float y2 = 1 - 2 * (y + h) / state.window_height;
+    float x1 = gl_x(x);
+    float y1 = gl_y(y);
+    float x2 = gl_x(x + w);
+    float y2 = gl_y(y + h);
 
     float r = (float)color.r / 255.0f;
     float g = (float)color.g / 255.0f;
@@ -195,7 +210,57 @@ void gl_draw_rect(float x, float y, float w, float h, Color color) {
     };
     GLuint index_data[] = { 0, 1, 2, 3, 0, 2 };
 
-    gl_draw_triangles(vertex_data, index_data);
+    gl_draw_triangles(vertex_data, index_data, 4, 2);
+}
+
+void gl_draw_circle_arc(float x, float y, float cr, float theta_start, float theta_arc, Color color) {
+    int n = (int)(40.0f * theta_arc / (2*M_PI));
+    GLfloat vertex_data[(n + 2) * 6];
+    GLuint index_data[n * 3];
+    int p = 0, i = 0;
+
+    float r = (float)color.r / 255.0f;
+    float g = (float)color.g / 255.0f;
+    float b = (float)color.b / 255.0f;
+    float a = (float)color.a / 255.0f;
+
+    vertex_data[p] = gl_x(x); p++;
+    vertex_data[p] = gl_y(y); p++;
+    vertex_data[p] = r; p++;
+    vertex_data[p] = g; p++;
+    vertex_data[p] = b; p++;
+    vertex_data[p] = a; p++;
+    vertex_data[p] = gl_x(x + cr * cosf(theta_start)); p++;
+    vertex_data[p] = gl_y(y - cr * sinf(theta_start)); p++;
+    vertex_data[p] = r; p++;
+    vertex_data[p] = g; p++;
+    vertex_data[p] = b; p++;
+    vertex_data[p] = a; p++;
+    for (int t = 0; t < n; t++) {
+        float theta = theta_start + (float)(t + 1) * theta_arc / (float)n;
+        vertex_data[p] = gl_x(x + cr * cosf(theta)); p++;
+        vertex_data[p] = gl_y(y - cr * sinf(theta)); p++;
+        vertex_data[p] = r; p++;
+        vertex_data[p] = g; p++;
+        vertex_data[p] = b; p++;
+        vertex_data[p] = a; p++;
+
+        index_data[i] = 0; i++;
+        index_data[i] = t + 1; i++;
+        index_data[i] = t + 2; i++;
+    }
+
+    gl_draw_triangles(vertex_data, index_data, n + 2, n);
+}
+
+void gl_draw_rounded_rect(float x, float y, float w, float h, float cr, Color color) {
+    gl_draw_rect(x + cr, y, w - 2*cr, h, color);
+    gl_draw_rect(x, y + cr, w, h - 2*cr, color);
+
+    gl_draw_circle_arc(x + w - cr, y + cr, cr, 0.0f, M_PI_2, color);
+    gl_draw_circle_arc(x + cr, y + cr, cr, M_PI_2, M_PI_2, color);
+    gl_draw_circle_arc(x + cr, y + h - cr, cr, M_PI, M_PI_2, color);
+    gl_draw_circle_arc(x + w - cr, y + h - cr, cr, M_PI + M_PI_2, M_PI_2, color);
 }
 
 int main(void) {
@@ -214,6 +279,11 @@ int main(void) {
                 case SDL_QUIT:
                     quit = true;
                     break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_q) {
+                        quit = true;
+                    }
+                    break;
             }
         }
 
@@ -221,6 +291,7 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
         gl_draw_rect(50.0f, 100.0f, 300.0f, 400.0f, {128, 128, 255, 255});
         gl_draw_rect(100.0f, 50.0f, 300.0f, 400.0f, {255, 128, 255, 255});
+        gl_draw_rounded_rect(150.0f, 150.0f, 300.0f, 400.0f, 40.0f, {255, 255, 255, 255});
         SDL_GL_SwapWindow(state.window);
     }
 
