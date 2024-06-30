b21e3215d06170c2180c2166194670ddb16789f2
Paul Page <pjpage98@gmail.com>
2024-06-30 08:00:18 -0500

    add more stuff to sdl demos

--- sdl-c/main.c
+++ sdl-c/main.c
@@ -18,21 +18,58 @@ int main(int argc, char *argv[]) {
     }
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0); SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
+    SDL_FRect rect1 = {0, 0, 100, 100};
+    SDL_FRect rect2 = {0, 0, 100, 100};
+    SDL_FRect rect3 = {0, 0, 100, 100};
+    bool show = false;
 
     bool quit = false;
     SDL_Event event;
     while (!quit) {
-        SDL_WaitEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_MOUSEWHEEL:
-                printf("wheel %f %f\n", event.wheel.preciseX, event.wheel.preciseY);
+        /*SDL_WaitEvent(&event);*/
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_MOUSEWHEEL:
+                    printf("wheel %f %f\n", event.wheel.preciseX, event.wheel.preciseY);
+                    rect1.x += event.wheel.preciseX * 10;
+                    rect1.y += event.wheel.preciseY * 10;
+                    break;
+                case SDL_MOUSEMOTION:
+                    rect2.x = event.motion.x;
+                    rect2.y = event.motion.y;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_q) {
+                        show = true;
+                    }
+                    break;
+                case SDL_KEYUP:
+                    if (event.key.keysym.sym == SDLK_q) {
+                        show = false;
+                    }
+                    break;
+            }
         }
+
+        rect1.y += 0.01;
 
         SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
         SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_RenderFillRectF(renderer, &rect1);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRectF(renderer, &rect2);
+
+        if (show) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 200, 255);
+            SDL_RenderFillRectF(renderer, &rect3);
+        }
+
         SDL_RenderPresent(renderer);
     }
 
--- sdl3-c/main.c
+++ sdl3-c/main.c
@@ -18,26 +18,66 @@ int main(int argc, char *argv[]) {
     }
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
+    SDL_FRect rect1 = {0, 0, 100, 100};
+    SDL_FRect rect2 = {0, 0, 100, 100};
+    SDL_FRect rect3 = {0, 0, 100, 100};
+    bool show = false;
+
     bool quit = false;
     SDL_Event event;
     while (!quit) {
-        SDL_WaitEvent(&event);
-        switch (event.type) {
-            case SDL_EVENT_QUIT:
-                quit = true;
-                break;
-            case SDL_EVENT_MOUSE_WHEEL:
-                printf("wheel %f %f\n", event.wheel.x, event.wheel.y);
-            case SDL_EVENT_MOUSE_BUTTON_DOWN:
-                size_t n = 0;
-                char *data = (char*)SDL_GetClipboardData("image/png", &n);
-                printf("data: %ld\n", n);
+        /*SDL_WaitEvent(&event);*/
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    quit = true;
+                    break;
+                case SDL_EVENT_MOUSE_WHEEL:
+                    printf("wheel %f %f\n", event.wheel.x, event.wheel.y);
+                    rect1.x += event.wheel.x * 10;
+                    rect1.y += event.wheel.y * 10;
+                    break;
+                /*case SDL_EVENT_MOUSE_BUTTON_DOWN:*/
+                /*    break;*/
+                    /*size_t n = 0;*/
+                    /*char *data = (char*)SDL_GetClipboardData("image/png", &n);*/
+                    /*printf("data: %ld\n", n);*/
+                case SDL_EVENT_MOUSE_MOTION:
+                    rect2.x = event.motion.x;
+                    rect2.y = event.motion.y;
+                    break;
+                case SDL_EVENT_KEY_DOWN:
+                    if (event.key.key == SDLK_q) {
+                        show = true;
+                    }
+                    break;
+                case SDL_EVENT_KEY_UP:
+                    if (event.key.key == SDLK_q) {
+                        show = false;
+                    }
+                    break;
+            }
         }
+
+        rect1.y += 0.01;
 
         SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
         SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &rect1);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect2);
+
+        if (show) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 200, 255);
+            SDL_RenderFillRect(renderer, &rect2);
+        }
+
         SDL_RenderPresent(renderer);
     }
 
+    SDL_Quit();
     return 0;
 }
