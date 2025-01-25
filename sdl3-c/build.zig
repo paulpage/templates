00c89e000bbe0f07cb146e3b8077c7ec5b21de32
Paul Page <pjpage98@gmail.com>
2025-01-24 22:56:33 -0600

    Add build.zig for sdl3-c

---
+++ sdl3-c/build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addCSourceFiles(.{ .files = &.{"main.c"} });
+    exe.linkLibC();
+    exe.linkSystemLibrary("sdl3");
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
