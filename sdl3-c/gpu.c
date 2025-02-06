dcb8c9f79c632ad870c1cc06c7502be3143a8553
Paul Page <pjpage98@gmail.com>
2025-02-05 23:39:30 -0600

    draw some text

--- sdl3-c/gpu.c
+++ sdl3-c/gpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -49,6 +50,16 @@ typedef struct Texture {
     int w, h, d;
 } Texture;
 
+#define FONT_SIZE 24.0f
+#define ATLAS_WIDTH 512
+#define ATLAS_HEIGHT 512
+
+typedef struct {
+    Texture texture;
+    stbtt_packedchar char_data[96];
+    float scale;
+} Font;
+
 typedef struct VertInput {
     Rect dst_rect;
     Rect src_rect;
@@ -60,6 +71,43 @@ typedef struct VertInput {
     float use_texture;
     float _padding[1]; // std140 alignment
 } VertInput;
+
+typedef struct VertStore {
+    VertInput *data;
+    int size;
+    int capacity;
+} VertStore;
+
+VertStore make_vert_store() {
+    VertInput *data = malloc(1024 * sizeof(VertInput));
+    return (VertStore){
+        .data = data,
+        .size = 0,
+        .capacity = 1024,
+    };
+}
+
+void push_vert(VertStore *store, VertInput input) {
+    if (store->size == store->capacity) {
+        printf("push capacity %d -> %d\n", store->capacity, store->capacity * 2);
+        store->capacity *= 2;
+        store->data = realloc(store->data, store->capacity * sizeof(VertInput));
+    }
+
+    store->data[store->size] = input;
+    store->size++;
+}
+
+void vert_clear(VertStore *store) {
+    store->size = 0;
+}
+
+void free_vert_store(VertStore *store) {
+    free(store->data);
+    store->data = NULL;
+    store->size = 0;
+    store->capacity = 0;
+}
 
 SDL_GPUShader *load_shader(
     SDL_GPUDevice *gpu,
@@ -89,16 +137,12 @@ SDL_GPUShader *load_shader(
     return shader;
 }
 
-Texture load_texture(SDL_GPUDevice *gpu, char *filename) {
-    int w, h, n;
-    unsigned char *data = stbi_load(filename, &w, &h, &n, 0);
-    ASSERT_CREATED(data);
-
+Texture load_texture_bytes(SDL_GPUDevice *gpu, u8 *data, int w, int h, int d) {
     SDL_GPUTransferBuffer *texture_transfer_buffer = SDL_CreateGPUTransferBuffer(
         gpu,
         &(SDL_GPUTransferBufferCreateInfo){
             .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-            .size = w * h * n,
+            .size = w * h * d,
         }
     );
 
@@ -107,21 +151,37 @@ Texture load_texture(SDL_GPUDevice *gpu, char *filename) {
         texture_transfer_buffer,
         false
     );
-    SDL_memcpy(texture_transfer_ptr, data, w * h * n);
+    SDL_memcpy(texture_transfer_ptr, data, w * h * d);
     SDL_UnmapGPUTransferBuffer(gpu, texture_transfer_buffer);
 
-    SDL_GPUTexture *handle = SDL_CreateGPUTexture(
-        gpu,
-        &(SDL_GPUTextureCreateInfo){
-            .type = SDL_GPU_TEXTURETYPE_2D,
-            .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
-            .width = w,
-            .height = h,
-            .layer_count_or_depth = 1,
-            .num_levels = 1,
-            .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
-        }
-    );
+    SDL_GPUTexture *handle;
+    if (d == 4) {
+        handle = SDL_CreateGPUTexture(
+            gpu,
+            &(SDL_GPUTextureCreateInfo){
+                .type = SDL_GPU_TEXTURETYPE_2D,
+                .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
+                .width = w,
+                .height = h,
+                .layer_count_or_depth = 1,
+                .num_levels = 1,
+                .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
+            }
+        );
+    } else if (d == 1) {
+        handle = SDL_CreateGPUTexture(
+            gpu,
+            &(SDL_GPUTextureCreateInfo){
+                .type = SDL_GPU_TEXTURETYPE_2D,
+                .format = SDL_GPU_TEXTUREFORMAT_A8_UNORM,
+                .width = w,
+                .height = h,
+                .layer_count_or_depth = 1,
+                .num_levels = 1,
+                .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
+            }
+        );
+    }
 
     SDL_GPUCommandBuffer *upload_cmd_buf = SDL_AcquireGPUCommandBuffer(gpu);
     SDL_GPUCopyPass *copy_pass = SDL_BeginGPUCopyPass(upload_cmd_buf);
@@ -145,16 +205,146 @@ Texture load_texture(SDL_GPUDevice *gpu, char *filename) {
     SDL_SubmitGPUCommandBuffer(upload_cmd_buf);
     SDL_ReleaseGPUTransferBuffer(gpu, texture_transfer_buffer);
 
-    stbi_image_free(data);
     return (Texture){
         .handle = handle,
         .w = w,
         .h = h,
-        .d = n,
+        .d = d,
     };
 }
 
+Texture load_texture(SDL_GPUDevice *gpu, char *filename) {
+    int w, h, n;
+    u8 *data = stbi_load(filename, &w, &h, &n, 0);
+    ASSERT_CREATED(data);
+
+    Texture texture = load_texture_bytes(gpu, data, w, h, n);
+
+    stbi_image_free(data);
+    return texture;
+}
+
+Font load_font(SDL_GPUDevice *gpu, const char* font_path) {
+    Font font = {0};
+    font.scale = FONT_SIZE;
+    size_t font_size = 0;
+    u8 *font_buffer = read_file(font_path, &font_size);
+    printf("font file size: %zd\n", font_size);
+
+    u8 *atlas_data = malloc(ATLAS_WIDTH * ATLAS_HEIGHT);
+
+    stbtt_pack_context pack_context = {0};
+
+    stbtt_pack_range pack_range = {0};
+    pack_range.font_size = FONT_SIZE;
+    pack_range.first_unicode_codepoint_in_range = 32;
+    pack_range.num_chars = 96;
+    pack_range.chardata_for_range = font.char_data;
+
+    stbtt_PackBegin(&pack_context, atlas_data, ATLAS_WIDTH, ATLAS_HEIGHT, 0, 1, NULL);
+    stbtt_PackFontRanges(&pack_context, font_buffer, 0, &pack_range, 1);
+
+    stbtt_PackEnd(&pack_context);
+
+	/*u32* pixels = malloc(ATLAS_WIDTH * ATLAS_HEIGHT * sizeof(u32));*/
+	/*const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(SDL_PIXELFORMAT_RGBA32);*/
+	/*for(int i = 0; i < ATLAS_WIDTH * ATLAS_HEIGHT; i++) {*/
+
+		/*pixels[i] = SDL_MapRGBA(format, NULL, 0xff, 0xff, 0xff, atlas_data[i]);*/
+	/*}*/
+
+    u8 *pixels = malloc(ATLAS_WIDTH * ATLAS_HEIGHT * 4);
+    for (int i = 0; i < ATLAS_WIDTH * ATLAS_HEIGHT; i++) {
+        pixels[i*4] = 0;
+        pixels[i*4 + 1] = 0;
+        pixels[i*4 + 2] = 0;
+        pixels[i * 4 + 3] = atlas_data[i];
+    }
+    /*font.texture = load_texture_bytes(gpu, atlas_data, ATLAS_WIDTH, ATLAS_HEIGHT, 1);*/
+    font.texture = load_texture_bytes(gpu, pixels, ATLAS_WIDTH, ATLAS_HEIGHT, 4);
+
+	free(pixels);
+
+    /*font.texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, ATLAS_WIDTH, ATLAS_HEIGHT);*/
+    /*SDL_SetTextureBlendMode(font.texture, SDL_BLENDMODE_BLEND);*/
+    /*SDL_UpdateTexture(font.texture, NULL, pixels, ATLAS_WIDTH * sizeof(Uint32));*/
+    /**/
+
+    free(atlas_data);
+    free(font_buffer);
+
+    return font;
+}
+
+void draw_text(VertStore *store, Font *font, const char *text, float x, float y) {
+    u8 r, g, b, a;
+    y += font->scale;
+    while (*text) {
+        if (*text >= 32 && *text < 128) {
+            stbtt_aligned_quad quad;
+            stbtt_GetPackedQuad(font->char_data, ATLAS_WIDTH, ATLAS_HEIGHT, *text - 32, &x, &y, &quad, 1);
+
+            VertInput vert = {
+                .dst_rect = (Rect){quad.x0, quad.y0, quad.x1 - quad.x0, quad.y1 - quad.y0},
+                /* .src_rect = (Rect){quad.s0 * ATLAS_WIDTH, quad.t0 * ATLAS_HEIGHT, (quad.s1 - quad.s0) * ATLAS_WIDTH, (quad.t1 - quad.t0) * ATLAS_HEIGHT}, */
+                /* .src_rect = (Rect){quad.s0, quad.t0, (quad.s1 - quad.s0), (quad.t1 - quad.t0)}, */
+                .src_rect = (Rect){quad.s0, quad.t0, (quad.s1 - quad.s0), (quad.t1 - quad.t0)},
+                .corner_radii = {0.0f, 0.0f, 0.0f, 0.0f},
+                .border_color = {1.0f, 1.0f, 1.0f, 1.0f},
+                .colors = {
+                    (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
+                    (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
+                    (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
+                    (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
+                },
+                .edge_softness = 1.0f,
+                .border_thickness = 1.0f,
+                .use_texture = 1.0f,
+            };
+            push_vert(store, vert);
+        }
+        text++;
+    }
+
+	/*   float start_x = x;*/
+	/**/
+	/*   Uint8 r, g, b, a;*/
+	/*SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a);*/
+	/*SDL_SetTextureColorMod(font->texture, r, g, b);*/
+	/*SDL_SetTextureAlphaMod(font->texture, a);*/
+	/**/
+	/*   while (*text) {*/
+	/*       if (*text >= 32 && *text < 128) {*/
+	/*           stbtt_aligned_quad quad;*/
+	/*           stbtt_GetPackedQuad(font->char_data, ATLAS_WIDTH, ATLAS_HEIGHT, *text - 32, &x, &y, &quad, 1);*/
+	/**/
+	/*           SDL_FRect src_rect = {*/
+	/*               quad.s0 * ATLAS_WIDTH,*/
+	/*               quad.t0 * ATLAS_HEIGHT,*/
+	/*               (quad.s1 - quad.s0) * ATLAS_WIDTH,*/
+	/*               (quad.t1 - quad.t0) * ATLAS_HEIGHT*/
+	/*           };*/
+	/**/
+	/*           SDL_FRect dst_rect = {*/
+	/*               quad.x0,*/
+	/*               quad.y0,*/
+	/*               quad.x1 - quad.x0,*/
+	/*               quad.y1 - quad.y0*/
+	/*           };*/
+	/**/
+	/*           SDL_RenderTexture(renderer, font->texture, &src_rect, &dst_rect);*/
+	/*       }*/
+	/*       ++text;*/
+	/*   }*/
+}
+
 int main(int argc, char *argv[]) {
+
+
+    size_t file_size = 0, line_count = 0;
+    char **buf = read_file_lines("render.c", &file_size, &line_count);
+    printf("file_size: %zd\nline_count: %zd\n", file_size, line_count);
+
 
     float scroll_offset = 0;
     bool mouse_down = false;
@@ -206,6 +396,7 @@ int main(int argc, char *argv[]) {
     SDL_ReleaseGPUShader(gpu, fragment_shader);
 
     // Textures
+    Font font = load_font(gpu, "../res/fonts/vera/Vera.ttf");
     Texture texture = load_texture(gpu, "../res/bird.png");
 
     SDL_GPUSampler *sampler = SDL_CreateGPUSampler(
@@ -220,11 +411,44 @@ int main(int argc, char *argv[]) {
         }
     );
 
+    // Buffer data
+    VertStore store = make_vert_store();
+
+    /* for (int i = 0; i < 100; i++) { */
+    /*     int x = (f32)(i / 10); */
+    /*     int y = (f32)(i % 10); */
+    /*     VertInput vert = { */
+    /*         .dst_rect = (Rect){x * 80.0f, y * 80.0f, 90.0f, 60.0f}, */
+    /*         .src_rect = (Rect){0.0f, 0.0f, 1.0f, 1.0f}, */
+    /*         .corner_radii = {30.0f, 30.0f, 10.0f, 5.0f}, */
+    /*         .border_color = {0.7f, 0.7f, 0.7f, 1.0f}, */
+    /*         .colors = { */
+    /*             (Vec4){1.0f, 1.0f, 1.0f, 1.0f}, */
+    /*             (Vec4){1.0f, 1.0f, 1.0f, 1.0f}, */
+    /*             (Vec4){1.0f, 1.0f, 1.0f, 1.0f}, */
+    /*             (Vec4){1.0f, 1.0f, 1.0f, 1.0f}, */
+    /*             /1* (Vec4){0.4f, 0.4f, 0.4f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.4f, 0.4f, 0.4f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.2f, 0.2f, 0.2f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.2f, 0.2f, 0.2f, 1.0f}, *1/ */
+    /*             /1* (Vec4){1.0f, x / 10.0f, y / 10.0f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.0f, 0.0f, 0.0f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.0f, 0.0f, 0.0f, 1.0f}, *1/ */
+    /*             /1* (Vec4){0.0f, 0.0f, 0.0f, 1.0f}, *1/ */
+    /*         }, */
+    /*         .edge_softness = 1.0f, */
+    /*         .border_thickness = 1.0f, */
+    /*         .use_texture = 1.0f, */
+    /*     }; */
+    /*     push_vert(&store, vert); */
+    /* } */
+
+
     SDL_GPUTransferBuffer *vertex_data_transfer_buffer = SDL_CreateGPUTransferBuffer(
         gpu,
         &(SDL_GPUTransferBufferCreateInfo){
             .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-            .size = 2 * sizeof(VertInput),
+            .size = store.capacity * sizeof(VertInput),
         }
     );
 
@@ -232,44 +456,46 @@ int main(int argc, char *argv[]) {
         gpu,
         &(SDL_GPUBufferCreateInfo){
             .usage = SDL_GPU_BUFFERUSAGE_GRAPHICS_STORAGE_READ,
-            .size = 2 * sizeof(VertInput),
+            .size = store.capacity * sizeof(VertInput),
         }
     );
 
-    // Buffer data
-    VertInput vertices[2] = {
-        (VertInput){
-            .dst_rect = (Rect){300.0f, 200.0f, 200.0f, 200.0f},
-            .src_rect = (Rect){0.0f, 0.0f, 32.0f/64.0f, 32.0f/64.0f},
-            .corner_radii = (Vec4){50.0f, 20.0f, 100.0f, 10.0f},
-            .border_color = (Vec4){1.0, 1.0, 0.0, 1.0},
-            .colors = {
-                (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
-                (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
-                (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
-                (Vec4){1.0f, 1.0f, 1.0f, 1.0f},
-            },
-            .edge_softness = 1.0f,
-            .border_thickness = 20.0f,
-            .use_texture = 1.0f,
-        },
-        (VertInput){
-            .dst_rect = (Rect){10.0f, 10.0f, 200.0f, 200.0f},
-            .src_rect = (Rect){0.0f, 0.0f, 1.0f, 1.0f},
-            .corner_radii = {10.0f, 10.0f, 10.0f, 10.0f},
-            .border_color = (Vec4){0.2, 0.2, 0.8, 1.0},
-            .colors = {
-                (Vec4){0.0f, 0.0f, 0.0f, 1.0f},
-                (Vec4){0.0f, 0.0f, 0.0f, 1.0f},
-                (Vec4){0.2f, 0.2f, 0.2f, 1.0f},
-                (Vec4){0.2f, 0.2f, 0.2f, 1.0f},
-            },
-            .edge_softness = 1.0f,
-            .border_thickness = 10.0f,
-        },
-    };
+    u64 buf_capacity = store.capacity;
 
-    unsigned int buf_size = 2 * sizeof(VertInput);
+    /*VertInput vertices[2] = {*/
+    /*    (VertInput){*/
+    /*        .dst_rect = (Rect){100.0f, 100.0f, 400.0f, 400.0f},*/
+    /*        .src_rect = (Rect){0.0f, 0.0f, 1.0f, 1.0f},*/
+    /*        .corner_radii = (Vec4){50.0f, 20.0f, 100.0f, 10.0f},*/
+    /*        .border_color = (Vec4){1.0, 1.0, 0.0, 1.0},*/
+    /*        .colors = {*/
+    /*            (Vec4){1.0f, 1.0f, 1.0f, 1.0f},*/
+    /*            (Vec4){1.0f, 1.0f, 1.0f, 1.0f},*/
+    /*            (Vec4){1.0f, 1.0f, 1.0f, 1.0f},*/
+    /*            (Vec4){1.0f, 1.0f, 1.0f, 1.0f},*/
+    /*        },*/
+    /*        .edge_softness = 1.0f,*/
+    /*        .border_thickness = 20.0f,*/
+    /*        .use_texture = 1.0f,*/
+    /*    },*/
+    /*    (VertInput){*/
+    /*        .dst_rect = (Rect){10.0f, 10.0f, 200.0f, 200.0f},*/
+    /*        .src_rect = (Rect){0.0f, 0.0f, 1.0f, 1.0f},*/
+    /*        .corner_radii = {10.0f, 10.0f, 10.0f, 10.0f},*/
+    /*        .border_color = (Vec4){0.2, 0.2, 0.8, 1.0},*/
+    /*        .colors = {*/
+    /*            (Vec4){0.0f, 0.0f, 0.0f, 1.0f},*/
+    /*            (Vec4){0.0f, 0.0f, 0.0f, 1.0f},*/
+    /*            (Vec4){0.2f, 0.2f, 0.2f, 1.0f},*/
+    /*            (Vec4){0.2f, 0.2f, 0.2f, 1.0f},*/
+    /*        },*/
+    /*        .edge_softness = 1.0f,*/
+    /*        .border_thickness = 10.0f,*/
+    /*    },*/
+    /*};*/
+    /**/
+
+    /*unsigned int buf_size = 2 * sizeof(VertInput);*/
 
     // Main loop
     bool quit = false;
@@ -307,6 +533,33 @@ int main(int argc, char *argv[]) {
             }
         }
 
+
+        store.size = 0;
+
+        for (int i = 0; i < line_count; i++) {
+            draw_text(&store, &font, buf[i], 0, i * 20 + scroll_offset);
+        }
+        if (buf_capacity != store.capacity) {
+            SDL_ReleaseGPUTransferBuffer(gpu, vertex_data_transfer_buffer);
+            vertex_data_transfer_buffer = SDL_CreateGPUTransferBuffer(
+                gpu,
+                &(SDL_GPUTransferBufferCreateInfo){
+                    .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+                    .size = store.capacity * sizeof(VertInput),
+                }
+            );
+            SDL_ReleaseGPUBuffer(gpu, vertex_data_buffer);
+            vertex_data_buffer = SDL_CreateGPUBuffer(
+                gpu,
+                &(SDL_GPUBufferCreateInfo){
+                    .usage = SDL_GPU_BUFFERUSAGE_GRAPHICS_STORAGE_READ,
+                    .size = store.capacity * sizeof(VertInput),
+                }
+            );
+            buf_capacity = store.capacity;
+        }
+
+
         SDL_GPUCommandBuffer *cmdbuf = SDL_AcquireGPUCommandBuffer(gpu);
         ASSERT_CREATED(cmdbuf);
 
@@ -317,11 +570,18 @@ int main(int argc, char *argv[]) {
 
             VertInput *data_ptr = SDL_MapGPUTransferBuffer(gpu, vertex_data_transfer_buffer, true);
 
+            for (int i = 0; i < store.size; i++) {
+                data_ptr[i] = store.data[i];
+            }
+            /*for (int i = 0; i < rect_count; i++) {*/
+            /*    data_ptr[i] = vertices[i];*/
+            /*}*/
+
             // modify data
             // data_ptr[i].dst_rect.x = ...
-            for (int i = 0; i < 2; i++) {
-                data_ptr[i] = vertices[i];
-            }
+            /*for (int i = 0; i < 2; i++) {*/
+            /*    data_ptr[i] = vertices[i];*/
+            /*}*/
 
             SDL_UnmapGPUTransferBuffer(gpu, vertex_data_transfer_buffer);
 
@@ -335,7 +595,7 @@ int main(int argc, char *argv[]) {
                 &(SDL_GPUBufferRegion) {
                     .buffer = vertex_data_buffer,
                     .offset = 0,
-                    .size = 2 * sizeof(VertInput),
+                    .size = store.size * sizeof(VertInput),
                 },
                 true
             );
@@ -361,7 +621,8 @@ int main(int argc, char *argv[]) {
                 render_pass,
                 0,
                 &(SDL_GPUTextureSamplerBinding){
-                    .texture = texture.handle,
+                    .texture = font.texture.handle,
+                    /* .texture = texture.handle, */
                     .sampler = sampler,
                 },
                 1
@@ -371,7 +632,7 @@ int main(int argc, char *argv[]) {
             SDL_PushGPUVertexUniformData(cmdbuf, 0, &(Vec2){800.0f, 600.0f}, sizeof(Vec2));
             SDL_PushGPUFragmentUniformData(cmdbuf, 0, &(Vec2){800.0f, 600.0f}, sizeof(Vec2));
 
-            SDL_DrawGPUPrimitives(render_pass, 2 * 6, 1, 0, 0);
+            SDL_DrawGPUPrimitives(render_pass, store.size * 6, 1, 0, 0);
             SDL_EndGPURenderPass(render_pass);
 
         }
--- sdl3-c/shaders/2d.frag.hlsl
+++ sdl3-c/shaders/2d.frag.hlsl
@@ -28,22 +28,20 @@ float4 main(Input input) : SV_Target0 {
         return input.color * texture.Sample(sam, input.tex_coord);
     }
 
-    // return input.color * texture.Sample(sam, input.tex_coord);
-
     float2 half_size = 2 * input.rect.zw / screen_size.y / 2;
     float2 p = (2 * input.position.xy - screen_size.xy) / screen_size.y - (2 * (input.rect.xy + input.rect.zw / 2) - screen_size.xy) / screen_size.y;
     float d = sdf_rounded_box(p, half_size, input.corner_radii / (screen_size.y / 2));
 
     float d2 = 0;
-    float2 half_size2 = 2 * (input.rect.zw - input.border_thickness * 2) / screen_size.y / 2;
+    float2 half_size2 = 2 * (input.rect.zw - (input.border_thickness * 2 + 2)) / screen_size.y / 2;
     if (input.border_thickness > 0) {
-        d2 = sdf_rounded_box(p, half_size2, (input.corner_radii - input.border_thickness) / (screen_size.y / 2));
+        d2 = sdf_rounded_box(p, half_size2, (input.corner_radii - (input.border_thickness + 2)) / (screen_size.y / 2));
     }
 
     float4 final_color = lerp(
-        float4(input.border_color.xyz, (1-smoothstep(0, 0.004, d)) * input.border_color.a),
+        float4(input.border_color.xyz, (1-smoothstep(0, 0.003, d)) * input.border_color.a),
         input.color,
-        1-smoothstep(0, 0.004, d2)
+        1-smoothstep(0, 0.005, d2)
     );
 
      return final_color;
